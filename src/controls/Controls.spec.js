6405ffd501290acc9b819afde6a589fbc6694880
c00kamunga <tsothalanti2@yahoo.com>
2020-01-17 16:16:59 -0800

    performed testing

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,32 @@
 // Test away!
+
+
+import React from 'react';
+import Controls from './Controls';
+import { render, fireEvent } from '@testing-library/react';
+import { exportAlldeclaration } from '@babel/types'
+describe('controls tests', ()=>{
+
+test('does CONTROLS render', ()=>{
+    render(<Controls/>)
+})
+
+test("if 'locked' prop is true, 'unlock gate' renders" , ()=>{
+const { getByText } = render(<Controls locked={}/>)
+getByText(/unlock gate/i)
+})
+
+test("buttons' text hanges to reflect the state the door will be in if clicked'", ()=>{
+    const { getByText } = render(<Dashboard/>)
+    const button = getByText(/open gate/i)
+    fireEvent.click(button)
+    getByText(/close gate/i)
+})
+
+})
+
+
+
+
+
+
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,15 @@
 // Test away
+
+
+
+
+import React from 'react';
+import Dashboard from './Dashboard'
+import {render} from '@testing-library/react';
+
+
+
+test("display 'closed' if the close prop is true", ()=>{
+    const App = render(<Dashboard/>)
+    expect(App).toMatchSnapshot()
+})
